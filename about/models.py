c9917d5ab0ef491ff8b1cef78ccd41dab8443b06
mahmoudsallem <salem7ouda@gmail.com>
2025-08-13 20:19:59 +0300

    add the About model

--- about/models.py
+++ about/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    What_we_do = models.CharField(max_length=200)
+    our_mission = models.TextField(max_length=10000, blank=True)
+    our_goals = models.TextField(max_length=10000, blank=True)
+    image = models.ImageField(upload_to='about_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.What_we_do
+    
+
+
+class FAQ(models.Model):
+    Title = models.CharField(max_length=200)
+    description = models.TextField(max_length=10000, blank=True)
+
+    def __str__(self):
+        return self.Title
+    
+
+
